97e4204f154b49f591750dc80875b1b664f99738
adilkhash <adil.khashtamov@gmail.com>
2023-09-24 17:39:26 +0600

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Django Trix Editor Integration
+
+This package provides a Django app that integrates the [Trix editor](https://trix-editor.org/) with your Django project.
---
+++ trix2/widgets.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.utils.html import html_safe
+from django.conf import settings
+
+TRIX_VERSION = settings.get('TRIX_VERSION', '2.0.6')
+
+
+@html_safe
+class JSPath:
+    def __str__(self):
+        return (
+            f'<script src="https://unpkg.com/trix@{TRIX_VERSION}/dist/trix.umd.min.js" rel="stylesheet">'
+        )
+
+
+@html_safe
+class CSSPath:
+    def __str__(self):
+        return (
+            f'<link rel="stylesheet" href="https://unpkg.com/trix@{TRIX_VERSION}/dist/trix.css">'
+        )
+
+
+class Trix2Widget(forms.Textarea):
+    def render(self, name, value, attrs=None, renderer=None):
+        attrs = attrs or {}
+        attrs["hidden"] = True
+        html = super().render(name, value, attrs=attrs, renderer=renderer)
+        return f'{html}<trix-editor input="{attrs["id"]}"></trix-editor>'
+
+    class Media:
+        js = [JSPath()]
+        css = {
+            "all": [CSSPath()],
+        }
