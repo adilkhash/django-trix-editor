f177aa0e7386922d77f26f0f9a4e1706eb98be1f
adilkhash <adil.khashtamov@gmail.com>
2023-09-27 12:01:55 +0600

    updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Django Trix Editor Integration
 
 This package provides a Django app that integrates the [Trix editor](https://trix-editor.org/) with your Django project.
+Requires Django 4.1+
 
 ## Installation
 
--- trix_editor/views.py
+++ trix_editor/views.py
@@ -1,13 +1,17 @@
+import typing as t
 from http import HTTPStatus
+from functools import wraps
 
 from django.views.decorators.http import require_POST
 from django.http.response import JsonResponse
+from django.http.request import HttpRequest
 from django.core.files.storage import default_storage
 from django.conf import settings
 
 
-def requires_permission(view_func):
-    def wrapper(request, *args, **kwargs):
+def requires_permission(view_func: t.Callable) -> t.Callable:
+    @wraps(view_func)
+    def wrapper(request: HttpRequest, *args, **kwargs):
         if permission := getattr(settings, 'TRIX_UPLOAD_PERMISSION', None):
             if not request.user.has_perm(permission):
                 return JsonResponse(
@@ -20,7 +24,7 @@ def requires_permission(view_func):
 
 @requires_permission
 @require_POST
-def handle_upload(request):
+def handle_upload(request: HttpRequest) -> JsonResponse:
     file = request.FILES.get('file')
     filename = default_storage.save(file.name, file)
     return JsonResponse({'attachment_url': default_storage.url(filename)})
