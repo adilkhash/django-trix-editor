a24a586f37995a31d4689370f45d3d59858773b4
Dmitry <d.akinin@gmail.com>
2025-10-11 18:55:00 +0300

    Update setup.py - version='0.4'

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [
 
 setup(
     name='django-trix-editor',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     url='https://github.com/adilkhash/django-trix-editor',
     license='MIT',
